5688ca049089ec20b42db8ab0bc7e5faf7c81501
Paddy Byers <paddy.byers@gmail.com>
2015-02-10 22:38:00 +0000

    Export classes needed to use Connection standalone

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 exports.Client = require('./lib/client.js');
+exports.Connection = require('./lib/connection.js');
 exports.types = require('./lib/types');
 exports.errors = require('./lib/errors.js');
 exports.policies = require('./lib/policies');
+exports.requests = require('./lib/requests');
+exports.utils = require('./lib/utils');
 exports.auth = require('./lib/auth');
